380be60b9aaf1b1b12eb1e7138680c01babcf99b
christian-nieves <thechristiannieves@gmail.com>
2026-04-07 15:29:46 -0400

    Step 1

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        // Question 1
+        String favoriteColor = "Green";
+        System.out.println("My Favorite Color is: " + favoriteColor);
+
+        int YearUpYear = 2026;
+        System.out.println("I Started Year Up in: " + YearUpYear);
+
+        String middlenameInitial = "M";
+        System.out.println("My Middle Name Initial is: " + middlenameInitial);
+
+        boolean hasPets = true;
+        System.out.println("Do I Have Any Pets?: " + hasPets);
+
+        String niceMessage = "You look great!";
+        System.out.println("Nice Message: " + niceMessage);
+    }
+}
